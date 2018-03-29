2ba23566247459a56e480befbcddf64304a3811d
Zp <zhaopanhw@foxmail.com>
2018-03-29 23:50:35 +0800

    完成orm数据库关系映射，并创建测试类

---
+++ src/test/java/com/imooc/demo/dao/AreaDaoTest.java
@@ -0,0 +1,34 @@
+package com.imooc.demo.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AreaDaoTest {
+
+
+    @Test
+    public void queryArea() {
+    }
+
+    @Test
+    public void queryAreaId() {
+    }
+
+    @Test
+    public void insertArea() {
+    }
+
+    @Test
+    public void updataArea() {
+    }
+
+    @Test
+    public void deleteArea() {
+    }
+}
